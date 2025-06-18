4b5811679246cea27a0783986606b489c16da3ae
Lindsay Ridgeway <LDRidgeway@gmail.com>
2025-06-18 13:45:12 -0400

    Initial commit for Russian Verbs of Action Practice App

---
+++ quizdata-tenses.js
@@ -0,0 +1,65 @@
+const verbData = [
+  {
+    prefix: "",
+    groups: [
+      {
+        tense: "past",
+        aspect: "perfective",
+        sentences: [
+          { english: "I went (male, on foot)", russian: "я пошёл" },
+          { english: "I went (female, on foot)", russian: "я пошла" },
+          { english: "he went (on foot)", russian: "он пошёл" },
+          { english: "she went (on foot)", russian: "она пошла" },
+          { english: "we went (on foot)", russian: "мы пошли" },
+          { english: "you went (on foot)", russian: "вы пошли" },
+          { english: "they went (on foot)", russian: "они пошли" },
+          { english: "I went (male, by vehicle)", russian: "я поехал" },
+          { english: "I went (female, by vehicle)", russian: "я поехала" },
+          { english: "he went (by vehicle)", russian: "он поехал" },
+          { english: "she went (by vehicle)", russian: "она поехала" },
+          { english: "we went (by vehicle)", russian: "мы поехали" },
+          { english: "they went (by vehicle)", russian: "они поехали" }
+        ]
+      },
+      {
+        tense: "past",
+        aspect: "imperfective",
+        sentences: [
+          { english: "I was going (on foot)", russian: "я ходил" },
+          { english: "he was going (on foot)", russian: "он ходил" },
+          { english: "she was going (on foot)", russian: "она ходила" },
+          { english: "we were going (on foot)", russian: "мы ходили" },
+          { english: "they were going (on foot)", russian: "они ходили" },
+          { english: "I was going (by vehicle)", russian: "я ездил" },
+          { english: "she was going (by vehicle)", russian: "она ездила" },
+          { english: "we were going (by vehicle)", russian: "мы ездили" },
+          { english: "they were going (by vehicle)", russian: "они ездили" }
+        ]
+      }
+    ]
+  },
+  {
+    prefix: "у",
+    groups: [
+      {
+        tense: "past",
+        aspect: "perfective",
+        sentences: [
+          { english: "I left (male, on foot)", russian: "я ушёл" },
+          { english: "I left (female, on foot)", russian: "я ушла" },
+          { english: "he left (on foot)", russian: "он ушёл" },
+          { english: "she left (on foot)", russian: "она ушла" },
+          { english: "we left (on foot)", russian: "мы ушли" },
+          { english: "you left (on foot)", russian: "вы ушли" },
+          { english: "they left (on foot)", russian: "они ушли" },
+          { english: "I left (male, by vehicle)", russian: "я уехал" },
+          { english: "I left (female, by vehicle)", russian: "я уехала" },
+          { english: "he left (by vehicle)", russian: "он уехал" },
+          { english: "she left (by vehicle)", russian: "она уехала" },
+          { english: "we left (by vehicle)", russian: "мы уехали" },
+          { english: "they left (by vehicle)", russian: "они уехали" }
+        ]
+      }
+    ]
+  }
+];
